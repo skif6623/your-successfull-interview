98a943c6311781923257606616df8bd9fde09b1b
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-02-07 18:38:50 +0200

    added adaptive button styles

--- src/components/ButtonList/ButtonList.styled.js
+++ src/components/ButtonList/ButtonList.styled.js
@@ -1,13 +1,17 @@
 import styled from "@emotion/styled";
+// import {FaHtml5, FaCss3Alt, FaReact} from "react-icons/fa";
+// import {DiJavascript1} from "react-icons/di";
 
 export const EButtonList = styled.ul`
 	display: flex;
 	flex-wrap: wrap;
-	gap: 30px;
-	outline: 1px solid red;
+	gap: 10px;
+	margin-top: 90px;
 `;
 
 export const EbuttonItem = styled.li`
-	width: 100%;
-	/* flex-basis: calc((100% - 30px) / 2); */
+	/* width: 100%; */
+	flex-basis: calc((100% - 10px) / 2);
 `;
+
+// export const EIcon = styled(FaHtml5);
--- src/components/ButtonList/ButtonList.tsx
+++ src/components/ButtonList/ButtonList.tsx
@@ -1,19 +1,42 @@
 import {Button} from "../Button/Button";
 import {Container, Typography} from "@mui/material";
 import {EButtonList, EbuttonItem} from "./ButtonList.styled";
+import {FaHtml5, FaCss3Alt, FaReact} from "react-icons/fa";
+import {DiJavascript1} from "react-icons/di";
 
 export const ButtonList = () => {
-	const buttonsNames = ["ALL", "HTML", "CSS", "JS", "React"];
+	const buttonsIcons = [
+		{
+			icon: <FaHtml5 size={40} />,
+			id: "html",
+			color: "#f46a3b",
+		},
+		{
+			icon: <FaCss3Alt size={40} />,
+			id: "css",
+			color: "#2196f3",
+		},
+		{
+			icon: <DiJavascript1 size={40} />,
+			id: "java script",
+			color: "#e6c830",
+		},
+		{
+			icon: <FaReact size={40} />,
+			id: "react",
+			color: "#00d8ff",
+		},
+	];
 	return (
 		<Container>
-			<Typography mt={15} mb={6} variant="h4" component="h1" textAlign="center">
-				Виберіть категорію
-			</Typography>
 			<EButtonList>
-				{buttonsNames.map(button => {
+				{buttonsIcons.map(({icon, id, color}) => {
 					return (
-						<EbuttonItem key={button}>
-							<Button color="red">{button}</Button>
+						<EbuttonItem key={id}>
+							<Button color={color}>
+								{icon}
+								<Typography textTransform="uppercase">{id}</Typography>
+							</Button>
 						</EbuttonItem>
 					);
 				})}
