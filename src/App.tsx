4e0de9095d6783fa4841c6fc1abe6dd8d1c670bb
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-02-08 23:04:49 +0200

    created sectionPage and added buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 // import { LoginPage } from "./pages/LoginPage";
 // import {RegistrationPage} from "./pages/RegistrationPage";
-import {Home} from "./pages/HomePage";
+// import {Home} from "./pages/HomePage";
+import {SectionPage} from "./pages/SectionPage";
 
 export const App = () => {
 	return (
 		<>
-			<Home />
+			<SectionPage />
 		</>
 	);
 };
